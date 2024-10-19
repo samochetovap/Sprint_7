c97ef034ce5b2fbb81c1120f3d664f65bf3ccdb2
samochetov_ap <near3228@gmail.com>
2024-10-19 11:03:02 +0300

    sprint_7

--- src/test/java/CourierCreateTest.java
+++ src/test/java/CourierCreateTest.java
@@ -16,21 +16,21 @@ public class CourierCreateTest {
     Courier courier;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         RestAssured.baseURI = "https://qa-scooter.praktikum-services.ru/";
         courier = new Courier("someLogin123321", "somePassword123321", "saske");
     }
 
     @Test
     @Step("Курьера можно создать")
-    public void createCourierTest(){
+    public void createCourierTest() {
         Response createResponse = createCourier(courier);
         createResponse.then().assertThat().statusCode(201).and().body("ok", is(true));
     }
 
     @Test
     @Step("Нельзя создать двух курьеров c одним и тем же логином")
-    public void cantCreateTwoEqualsCouriersTest(){
+    public void cantCreateTwoEqualsCouriersTest() {
         Response firstCreateResponse = createCourier(courier);
         firstCreateResponse.then().assertThat().statusCode(201).and().body("ok", is(true));
         Response secondCreateResponse = createCourier(courier);
@@ -38,20 +38,21 @@ public class CourierCreateTest {
     }
 
     @Test
-    @Step("Запрос без логина или пароля")
-    public void checkCreateDataTest(){
-        Courier courierWithNullLogin = new Courier(null, "test", "21312312");
+    @Step("Создание учетной записи без логина или пароля")
+    public void checkCreateDataTest() {
+        Courier courierWithNullLogin = new Courier(null, "test", "name");
         Response withNullLoginResponse = createCourier(courierWithNullLogin);
         withNullLoginResponse.then().assertThat().statusCode(400).and().body("message", is("Недостаточно данных для создания учетной записи"));
-        Courier courierWithNullPassword = new Courier("test", null, "21312312");
+        Courier courierWithNullPassword = new Courier("test", null, "name");
         Response withNullPasswordResponse = createCourier(courierWithNullPassword);
         withNullPasswordResponse.then().assertThat().statusCode(400).and().body("message", is("Недостаточно данных для создания учетной записи"));
+        //на всякий случай попытаемся удалить если через api ошибочно создадутся курьеры
         deleteCourierIfExists(courierWithNullLogin);
         deleteCourierIfExists(courierWithNullPassword);
     }
 
     @Step("Создание курьера")
-    public Response createCourier(Courier courier){
+    public Response createCourier(Courier courier) {
         Response response = given()
                 .contentType(ContentType.JSON)
                 .and()
@@ -63,21 +64,21 @@ public class CourierCreateTest {
 
 
     @Step("Удаление курьера если он есть")
-    public void deleteCourierIfExists(Courier courier){
+    public void deleteCourierIfExists(Courier courier) {
         Response response = given()
                 .contentType(ContentType.JSON)
                 .and()
                 .body(courier)
                 .and()
                 .post("/api/v1/courier/login");
-        if(response.statusCode() == 200){
+        if (response.statusCode() == 200) {
             String courierId = response.then().extract().body().path("id").toString();
             courier.setId(courierId);
             deleteCourier(courier);
         }
     }
 
-    private Response deleteCourier(Courier courier){
+    private Response deleteCourier(Courier courier) {
         Response response = given()
                 .contentType(ContentType.JSON)
                 .and()
@@ -86,8 +87,9 @@ public class CourierCreateTest {
                 .delete("/api/v1/courier/" + courier.getId());
         return response;
     }
+
     @After
-    public void deleteCourierIfExists(){
+    public void deleteCourierIfExists() {
         deleteCourierIfExists(courier);
     }
 
